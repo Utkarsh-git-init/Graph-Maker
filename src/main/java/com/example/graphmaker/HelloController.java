b5473d6cbad80cf8943da05195296be19e585d48
Utkarsh-git-init <utmaurya2312@gmail.com>
2025-08-23 01:43:26 +0530

    fixed ui, added axes coordinates, solved special cases(0 input cases)

--- src/main/java/com/example/graphmaker/HelloController.java
+++ src/main/java/com/example/graphmaker/HelloController.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 import javafx.stage.Screen;
 
 import java.io.IOException;
@@ -21,19 +22,70 @@ public class HelloController {
     @FXML
     public TextField Constant;
     @FXML
-    public void passequation(Scene2Controller controller2){
-        int xco=Integer.parseInt(XCoeff.getText());
-        int yco=Integer.parseInt(YCoeff.getText());
-        int cons=Integer.parseInt(Constant.getText());
-        controller2.equation(xco,yco,cons);
+    // variables that store coefficient in int
+    int []coeff=new int[3];
+    boolean []check=new boolean[3]; // variable that check if above are initialized
+    public Label xerror;
+    public Label yerror;
+    public Label conserror;
+    public boolean isInteger(String s){
+        return s.matches("-?\\d+");
+    }
+    public void isvalid(String s,int itr,Label label){
+        if(s.isBlank()){
+            label.setText("Empty field not allowed");
+        }else{
+            if(isInteger(s)){
+                coeff[itr]=Integer.parseInt(s);
+                label.setText("Correct input");
+                check[itr]=true;
+            }else{
+                label.setText("Enter only Integers");
+            }
+        }
+    }
+    public boolean passequation(Scene2Controller controller2) throws IOException {
+        // checking if input is valid or not
+        isvalid(XCoeff.getText(),0,xerror);
+        isvalid(YCoeff.getText(),1,yerror);
+        isvalid(Constant.getText(),2,conserror);
+        if(check[0]&&check[1]&&check[2]){
+            if(coeff[0]==0&&coeff[1]==0){
+                //write code for if coeffx and coeffy is 0
+                AnchorPane root=new FXMLLoader().load(getClass().getResource("Special_case.fxml"));
+                Label special_label=new Label();
+                if(coeff[2]!=0) {
+                    special_label.setText("Equation is invalid (no solution)");
+                    special_label.setLayoutX(225);
+                    special_label.setLayoutY(360);
+                }
+                else {
+                    special_label.setText("Equation is trivial (every point is a solution)");
+                    special_label.setLayoutX(170);
+                    special_label.setLayoutY(360);
+                }
+                special_label.setFont(Font.font(special_label.getFont().getFamily(),25));
+                root.getChildren().add(special_label);
+                Scene scene3=new Scene(root);
+                HelloApplication.primarystage.setScene(scene3);
+                return false;
+            }else
+            {
+                controller2.equation(coeff[0],coeff[1],coeff[2]);
+                return true;
+            }
+        }
+        else
+            return false;
     }
     public void switchtoScene2(ActionEvent e) throws IOException {
         FXMLLoader loader=new FXMLLoader(HelloApplication.class.getResource("Second-Scene.fxml"));
         AnchorPane root=loader.load();
         Scene2Controller controller2=loader.getController();
-        passequation(controller2);
-        //Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
-        Scene scene2=new Scene(root);
-        HelloApplication.primarystage.setScene(scene2);
+        boolean to_set_scene=passequation(controller2);
+        if(to_set_scene){
+            Scene scene2=new Scene(root);
+            HelloApplication.primarystage.setScene(scene2);
+        }
     }
 }
--- src/main/java/com/example/graphmaker/Scene2Controller.java
+++ src/main/java/com/example/graphmaker/Scene2Controller.java
@@ -1,19 +1,46 @@
 package com.example.graphmaker;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Line;
 
 public class Scene2Controller {
+    public Label x100;
+    public Label x200;
+    public Label x300;
+    public Label xn100;
+    public Label xn200;
+    public Label xn300;
+    public Label y100;
+    public Label y200;
+    public Label y300;
+    public Label yn100;
+    public Label yn200;
+    public Label yn300;
+
     @FXML
     private AnchorPane root;
     public void equation(int xcoeff,int ycoeff,int constant){
         //will take a value of x here and calc y
         // and pass it to next function to print graph
-        int x1,x2,y1,y2;
-        x1=-300;x2=300;
-        y1=(constant-(x1*xcoeff))/ycoeff;
-        y2=(constant-(x2*xcoeff))/ycoeff;
+        int x1=0,x2=0,y1=0,y2=0;
+        if(ycoeff!=0&&xcoeff!=0){
+            x1=-400;x2=400;
+            y1=(constant-(x1*xcoeff))/ycoeff;
+            y2=(constant-(x2*xcoeff))/ycoeff;
+        }else{
+            if(xcoeff==0){
+                x1=-400;x2=400;
+                y1=constant/ycoeff;
+                y2=y1;
+            }
+            if(ycoeff==0){
+                y1=400;y2=-400;
+                x1=constant/xcoeff;
+                x2=x1;
+            }
+        }
         graph(x1,x2,y1,y2);
     }
     public void graph(int x1,int x2,int y1,int y2){
@@ -35,5 +62,19 @@ public class Scene2Controller {
         y2=400-y2;
         Line line=new Line(x1,y1,x2,y2);//line is going out of the window
         root.getChildren().add(line);
+
+        // marking coordinates on x and y axes
+        x100.setLayoutX(100+400);  x100.setLayoutY(400);
+        x200.setLayoutX(200+400);  x200.setLayoutY(400);
+        x300.setLayoutX(300+400);  x300.setLayoutY(400);
+        xn100.setLayoutX(400-100);  xn100.setLayoutY(400);
+        xn200.setLayoutX(400-200);  xn200.setLayoutY(400);
+        xn300.setLayoutX(400-300);  xn300.setLayoutY(400);
+        y100.setLayoutX(400);  y100.setLayoutY(400-100);
+        y200.setLayoutX(400);  y200.setLayoutY(400-200);
+        y300.setLayoutX(400);  y300.setLayoutY(400-300);
+        yn100.setLayoutX(400);  yn100.setLayoutY(400+100);
+        yn200.setLayoutX(400);  yn200.setLayoutY(400+200);
+        yn300.setLayoutX(400);  yn300.setLayoutY(400+300);
     }
 }
